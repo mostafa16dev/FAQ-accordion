1a0648ef57058f5c79cd9ab4c267738aa4a1dfd0
mostafa16dev <36781239+mostafa16dev@users.noreply.github.com>
2024-02-25 10:45:12 +0200

    Update main.js get display style bug fix

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const toggleAccordion = (e) => {    
     const targetedAccordion = e.srcElement.attributes[2].nodeValue;
     const accordionBody = document.getElementById(targetedAccordion);
-    if (accordionBody.style.display === 'none' || accordionBody.style.display === '') {
+    if (getStyle(accordionBody) === 'none' ) {
         accordionBody.style.display = 'block';
         e.srcElement.style.setProperty('--url', "url('assets/images/icon-minus.svg')");
     } else {
         accordionBody.style.display = 'none';
         e.srcElement.style.setProperty('--url', "url('assets/images/icon-plus.svg')");
     }
+}
+
+const getStyle = element => {
+    return element.currentStyle ? element.currentStyle.display :
+        getComputedStyle(element, null).display;
 }
 
 const accordionHeadingOne = document.getElementById('headingOne').addEventListener("click", toggleAccordion);
